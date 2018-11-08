9707d3373282a13610dcc4e83d515db88f524c93
Eliiisa <elisaklein66@gmail.com>
2018-11-08 14:13:06 +0100

    test .idea

--- views/accueil.php
+++ views/accueil.php
@@ -3,7 +3,7 @@
 		<div class="col-12">
 			<h1>Titre Accueil</h1>
 			<p>Texte ici</p>
-			<a href="index.php?p=inscription" class="btn btn-primary btn-sm">GO !</a>
+			<a href="index.php?p=inscription" class="btn btn-primary btn-sm">GO ! </a>
 		</div>
 	</div>
 </div>
