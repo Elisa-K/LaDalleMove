d5fbbf4c1dae8c5e4e42cb0150ff7f0b0341f0eb
Eliiisa <elisaklein66@gmail.com>
2018-11-08 14:42:25 +0100

    préparation des vues

--- index.php
+++ index.php
@@ -10,8 +10,17 @@ if (isset($_REQUEST['p'])) {
 
 
 switch($page){
-	case 'accueil':
-
+	case 'accueil':;
+		$vue = 'views/accueil.php';
+	break;
+	case 'inscription':
+		$vue = 'views/inscription.php';
+	break;
+	case 'carte':
+		$vue = 'views/carte.php';
+	break;
+	case 'resultat':
+		$vue = 'views/resultat.php';
 		break;
 	default:
 		$vue = 'views/accueil.php';
---
+++ views/inscription.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <form action="">
+                <div class="form-group">
+                    <label for="">Votre pseudo</label>
+                    <input type="text" class="form-control">
+                </div>
+                <a href="index.php?p=carte" class="btn btn-primary">Valider</a>
+            </form>
+
+        </div>
+    </div>
+</div>
